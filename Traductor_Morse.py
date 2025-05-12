bd8833983621f99cbf4949d9d61ba2b6aea1f8d1
Samuel <estudiante1033491409@gmail.com>
2025-05-12 17:04:08 -0500

    Defino funciones, corrijo el final de cada caracter morse eliminando "/", agrego caracter espacio, adiciono la opción de traducir de morse a texto y agrego un bucle para que el usuario interactue con el programa.

--- Traductor_Morse.py
+++ Traductor_Morse.py
@@ -1,42 +1,61 @@
-"""
-Esta es la primera versión del programa traductor de texto a morse:
+#Se define la función que traduce de texto a morse
 
-    En esta versión se pueden traducir caracteres desde la A hasta la Z, 
-    no permite dejar espacio entre las letras y en caso de que el usuario
-    desee traducir un caracter que no se encuentra en la lista de letras,
-    el programa no le permitirá continuar con la traducción del texto.
-"""
-
-#Se le pregunta al usuario que texto desea traducir:
-
-texto = input("Ingrese el texto que desea traducir a código morse:\n")
-
-#Las siguientes dos listas permiten conocer la respectiva traducción a morse de cada letra:
-
-caracteres_morse = [".- .-.-./ ","-... .-.-./ ","-.-. .-.-./ ","-.. .-.-./ ",". .-.-./ ","..-. .-.-./ ","--. .-.-./ ",
-                        ".... .-.-./ ",".. .-.-./ ",".--- .-.-./ ","-.- .-.-./ ",".-.. .-.-./ ","-- .-.-./ ","-. .-.-./ ",
-                        "--- .-.-./ ",".--. .-.-./ ","--.- .-.-./ ",".-. .-.-./ ","... .-.-./ ","- .-.-./ ","..- .-.-./ ",
-                        "...- .-.-./ ",".-- .-.-./ ","-..- .-.-./ ","-.-- .-.-./ ","--.. .-.-./ "]
-letras = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-
-#La variable traduccion almacena la traducción del texto
-
-traduccion = ""
-
-"""El búcle for realiza una búsqueda dentro de la lista letras,
-comparando cada caracter dentro del texto del usuario con los caracteres de la lista letras,
-almacenando su respectiva traducción en caracteres_morse dentro de traduccion"""
-
-for i in range(len(texto)):
-    if(texto[i].upper() in letras):
-        w = 0
-        while(texto[i].upper() != letras[w]):
-            w += 1
+def texto_a_morse(msg):
+    caracteres_morse = [".- .-.-. ","-... .-.-. ","-.-. .-.-. ","-.. .-.-. ",". .-.-. ","..-. .-.-. ","--. .-.-. ",
+                        ".... .-.-. ",".. .-.-. ",".--- .-.-. ","-.- .-.-. ",".-.. .-.-. ","-- .-.-. ","-. .-.-. ",
+                        "--- .-.-. ",".--. .-.-. ","--.- .-.-. ",".-. .-.-. ","... .-.-. ","- .-.-. ","..- .-.-. ",
+                        "...- .-.-. ",".-- .-.-. ","-..- .-.-. ","-.-- .-.-. ","--.. .-.-. ","/ "]
+    letras = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"," ","¿","?","¡","!"]
+    traduccion = ""
+    for i in range(len(msg)):
+        if(msg[i].upper() in letras):
+            w = 0
+            while(msg[i].upper() != letras[w]):
+                w += 1
+            else:
+                traduccion += caracteres_morse[w]
         else:
-            traduccion += caracteres_morse[w]
-    else:
-        traduccion = "Lo sentimos realizar la traducción :("
+            traduccion = "Lo sentimos no se pudo realizar la traducción :("
+            break
+    return traduccion
 
-#Se imprime el resultado final de la traducción del texto ingresado por el usuario
+#Se define la función que traduce de morse a texto
 
-print(traduccion)
+def morse_a_texto(msg):
+    caracteres_morse = [".- .-.-. ","-... .-.-. ","-.-. .-.-. ","-.. .-.-. ",". .-.-. ","..-. .-.-. ","--. .-.-. ",
+                        ".... .-.-. ",".. .-.-. ",".--- .-.-. ","-.- .-.-. ",".-.. .-.-. ","-- .-.-. ","-. .-.-. ",
+                        "--- .-.-. ",".--. .-.-. ","--.- .-.-. ",".-. .-.-. ","... .-.-. ","- .-.-. ","..- .-.-. ",
+                        "...- .-.-. ",".-- .-.-. ","-..- .-.-. ","-.-- .-.-. ","--.. .-.-. ","/ "]
+    letras = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"," "]
+    traduccion = ""
+    morse = []
+    temp = ""
+    for i in range(len(msg)):
+        temp += msg[i]
+        if(temp in caracteres_morse):
+            morse.append(temp)
+            temp = "" 
+    for i in range(len(morse)):
+        if(morse[i] in caracteres_morse):
+            w = 0
+            while(morse[i] != caracteres_morse[w]):
+                w +=1
+            else:
+                traduccion += letras[w]
+    return traduccion
+
+#Se crea un bucle para que el usuario decida cuando quiere dejar de traducir
+
+repetir = "SI"
+while(repetir == "SI"):
+    pregunta = input("Seleccione:\nPara traducir de texto a morse, ingrese 1\nPara traducir de morse a texto, ingrese 2\n")
+    if(pregunta == "1"):
+        texto = input("Ingrese el texto que desea traducir a código morse:\n")
+        print(texto_a_morse(texto))
+    elif(pregunta == "2"):
+        texto = input("Ingrese el código morse que desea traducir a texto:\n")
+        print(morse_a_texto(texto))
+    repetir = input("¿Desea volver a traducir? (Responda con si o no)\n").upper()
+    if(repetir == "NO"):
+        print("¡Vueva pronto! :)")
+        break
